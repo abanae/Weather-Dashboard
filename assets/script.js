add2a4156f8e3b18f57cad236f71038ef8d0e942
Ana Ea <aabad0464@gmail.com>
2021-05-25 16:51:47 -0600

    function Api

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+let cityTextEl = document.querySelector(`#city`);
+let  weatherInputEl= document.querySelector(`#cards`);
+let buttonEl = document.querySelector(`#searchBtn`);
+let apiKey = `5ab6745ea373fe100ae9cd6aa16bdfa4`;
+let city = cityTextEl.value;
+
+
+// $(".saveBtn").on("click", function () {
+    
+// }
+
+
+function getApi(){
+let urlApi = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+fetch(urlApi)
+.then(function(response){
+    response.json().then(function(data){
+    }
+    )
+}) 
+
+}
