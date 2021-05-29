52ded4b53661bc0e5fc6300f1abee6b65561748a
Ana Ea <aabad0464@gmail.com>
2021-05-28 20:28:24 -0600

    function to get weather

--- assets/script.js
+++ assets/script.js
@@ -29,14 +29,13 @@ function displayWeather(event){
     }
 }
 
-// // Here we create the AJAX call
-// function currentWeather(city){
-//     // Here we build the URL so we can get a data from server side.
-//     var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey ;
-//     $.ajax({
-//         url:queryURL,
-//         method:"GET",
-//     }).then(function(response){
+// GET Function
+function currentWeather(city){
+    var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey ;
+    $.ajax({
+        url:queryURL,
+        method:"GET",
+    }).then(function(response){
 
 //         // parse the response to display the current weather including the City name. the Date and the weather icon. 
 //         console.log(response);
